2ab040367a8bd6617c7c721cac06554f290ccd9d
monteixy <yann.mx18@gmail.com>
2014-12-03 19:05:33 +0100

    Create ajout_joueur.java
    voila la fonction qui permer d'ajouter un joueur tout en incrementant l'ID de 1 automatiquement (#swaaaag)

---
+++ src/nim/ajout_joueur.java
@@ -0,0 +1,60 @@
+public class ajout_joueur {
+	
+	public static int nbJoueurs(){
+		try{ 
+				Class.forName("org.postgresql.Driver"); // charge le driver
+				String url = "jdbc:postgresql://localhost:5432/Projet_NIM"; // URL de la base
+				String user = "postgres";  // Nom de la base
+				String passwd = "3210Azerty"; // mot de passe de la base
+				Connection conn = DriverManager.getConnection(url, user, passwd);// connecte la base à l'application 
+				
+				ResultSet Requete = null; // indispensable pour le count (sinom java pas content)
+				Statement state = conn.createStatement();
+				Requete = state.executeQuery("SELECT COUNT(*) FROM joueurs");
+				if (Requete.next()){
+				int nb = Requete.getInt(1); // affectation de Count par la requete
+				return nb	;	
+				}
+		}
+	     catch (Exception e) { // leve une exception en cas d'erreur
+		      e.printStackTrace();	
+		      
+	    }
+		return -1;	
+	}
+	
+	   public static boolean ajouterNouveauJoueur(int n, String pseudo,String mdp){
+		   try {
+			      Class.forName("org.postgresql.Driver"); // charge le driver
+
+			      String url = "jdbc:postgresql://localhost:5432/Projet_NIM"; // URL de la base
+			      String user = "postgres";  // Nom de la base
+			      String passwd = "3210Azerty"; // mot de passe de la base
+
+			      Connection conn = DriverManager.getConnection(url, user, passwd);// connecte la base à l'application mais le transfert de donnée n'est pas encore permis
+			      
+			      String requete = "INSERT INTO joueurs(num_joueur, nb_parties_jouees, pseudo_joueur , nb_victoires , mot_de_passe) VALUES(?,?,?,?,?)";
+			      PreparedStatement pst = conn.prepareStatement(requete);
+			      pst.setInt(1, n);
+			      pst.setInt(2, 0);
+			      pst.setString(3, pseudo);
+			      pst.setInt(4, 0);
+		          pst.setString(5, mdp);
+		          pst.executeUpdate();
+			      return true;
+			    }
+			    catch (Exception e) { // leve une exception en cas d'erreur
+				      e.printStackTrace();		      
+				      
+			    }
+		   return false;
+	   }
+	   public static void main(String[] args) {   
+		   
+		  int n=nbJoueurs()+1;
+		  if( ajouterNouveauJoueur(n,"Garen","Demacia")){
+			  System.out.println("Joueur ajouté");
+		  }
+		   
+	}
+}
